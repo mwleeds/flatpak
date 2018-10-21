0ae156e0b94462a5e7ea3e11faa04a09a6085aa8
Matthew Leeds <matthew.leeds@endlessm.com>
2018-10-21 16:58:23 -0700

    commit: Remove misleading "Since:" annotations
    Thes are leftover from when these functions were copied from GLib.

--- common/flatpak-utils.c
+++ common/flatpak-utils.c
@@ -686,8 +686,6 @@ is_valid_name_character (gint c, gboolean allow_dash)
  * 2) DBus names require only two elements
  *
  * Returns: %TRUE if valid, %FALSE otherwise.
- *
- * Since: 2.26
  */
 gboolean
 flatpak_is_valid_name (const char *string,
@@ -952,8 +950,6 @@ is_valid_branch_character (gint c)
  * Branch names must contain at least one character.
  *
  * Returns: %TRUE if valid, %FALSE otherwise.
- *
- * Since: 2.26
  */
 gboolean
 flatpak_is_valid_branch (const char *string,
